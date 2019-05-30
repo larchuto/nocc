f7c15e94c8c29e29baf74e0c119b30794dd12b29
larchuto <larchuto@users.noreply.github.com>
2019-05-30 16:14:07 +0200

    add dialog marker detection and cleaning
    this allow to be deterministic

--- nocc/subtitles_utils.py
+++ nocc/subtitles_utils.py
@@ -36,6 +36,31 @@ def remove_lyrics(subtitle_text, lyrics_tokens_pair, start):
             subtitle_text = ""
     return subtitle_text
 
+
+def detect_dialog_marker(subtitles, dialog_marker_candidates):
+    candidate_count = {}
+    for candidate in dialog_marker_candidates:
+        candidate_count[candidate] = 0
+        candidate_count[candidate+"\s"] = 0
+
+    for subtitle in subtitles:
+        for key in candidate_count.keys():
+            count = len(re.findall("(?m)^" + re.escape(key), subtitle.text))
+            candidate_count[key] += count
+
+    # get dictionary key with maximum count
+    marker = max(candidate_count, key=candidate_count.get)
+    marker = re.sub("\\\s", " ", marker)
+    return marker
+
+
+def clean_dialog_marker(subtitle_text,
+                        possible_dialog_markers,
+                        dialog_marker_to_use):
+    pattern = "(?m)^[{}]\s?".format(re.escape(''.join(possible_dialog_markers)))
+    subtitle_text = re.sub(pattern, dialog_marker_to_use, subtitle_text)
+    return subtitle_text
+
 def clean_single_dialog(subtitle_text):
     return re.sub("^\s?[\-‐]\s?(.*)\Z", "\g<1>", subtitle_text)
 
@@ -44,8 +69,13 @@ def clean_newlines(subtitle_text):
 
 
 def clean_subtitles(subtitles, tokens, lyrics_tokens):
+    dialog_marker_list = ["‐", "-", "—"]
+    dialog_marker = detect_dialog_marker(subtitles, dialog_marker_list)
     for subtitle in subtitles:
         subtitle.text = clean_newlines(subtitle.text)
+        subtitle.text = clean_dialog_marker(subtitle.text,
+                                            dialog_marker_list,
+                                            dialog_marker)
         subtitle.text = remove_non_dialog(subtitle.text, tokens + lyrics_tokens)
 
     for lyrics_tokens_pair in lyrics_tokens:
