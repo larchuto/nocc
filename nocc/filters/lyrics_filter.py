12aab762c40ca205fe5375fe716e3f4659f022f5
larchuto <larchuto@users.noreply.github.com>
2019-05-31 00:38:38 +0200

    simplify a bit lyrics removal

--- nocc/filters/lyrics_filter.py
+++ nocc/filters/lyrics_filter.py
@@ -1,27 +1,24 @@
 import re
 
-def remove_lyrics_from_text(subtitle_text, lyrics_tokens_pair, start):
-    if start:
-        remove_lyrics.in_lyrics = False
-    if not remove_lyrics.in_lyrics:
+def remove_lyrics_from_text(subtitle_text, lyrics_tokens_pair, in_lyrics):
+    if not in_lyrics:
         regexp = "(?:{})+(?:.|\n)*".format(re.escape(lyrics_tokens_pair[0]))
         subtitle_text, nb_match = re.subn(regexp, "", subtitle_text, count = 1)
         if nb_match > 0:
-            remove_lyrics.in_lyrics = True
+            in_lyrics = True
     else: # in lyrics
         regexp = "(?:.|\n)*(?:{})+".format(re.escape(lyrics_tokens_pair[1]))
         subtitle_text, nb_match = re.subn(regexp, "", subtitle_text, count = 1)
         if nb_match > 0:
-            remove_lyrics.in_lyrics = False
+            in_lyrics = False
         else:
             subtitle_text = ""
-    return subtitle_text
+    return subtitle_text, in_lyrics
 
 def remove_lyrics(subtitles, lyrics_tokens):
     for lyrics_tokens_pair in lyrics_tokens:
-        start = True
+        in_lyrics = False
         for subtitle in subtitles:
-            subtitle.text = remove_lyrics_from_text(subtitle.text,
-                                                    lyrics_tokens_pair,
-                                                    start)
-            start = False
+            subtitle.text, in_lyrics = remove_lyrics_from_text(subtitle.text,
+                                                        lyrics_tokens_pair,
+                                                        in_lyrics)
